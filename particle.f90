27857da3c5c9c4a296d571d4ad6db125cc6ae1b0
alefrancc <153271933+alefrancc@users.noreply.github.com>
2024-01-04 06:35:42 +0200

    change

--- particle.f90
+++ particle.f90
@@ -1,11 +1,12 @@
 module particle
    implicit none
    type :: par
-      real :: q,m,f(3),pos(3),v(3),a(3) ! I added q and f(3)
+      real :: q,m,f(3),pos(3),v(3),a(3) ! charge, mass, force exerted, position, velocity, acceleration 
    end type par
+   
 contains
-   ! small change of order
 
+   ! cross product of two 3D vectors - function
    function cProd(v1,v2) result(retval)
       real, intent(in) :: v1(3),v2(3)
       real :: retval(3)
@@ -16,15 +17,15 @@ contains
       return
    end function cProd
 
+   ! time step subroutine (time step = 1e-10 ; it is equal 0.1ns)
    subroutine step(p,E,B)
       type (par), intent(inout) :: p
       real, dimension(3), intent(inout) :: E,B
 
-      p%f = p%q*(E+cProd(p%v,B)) ! I add f to subroutine since it has to be calculated everytime anyway
-      p%a = p%f/p%m
-      p%v = p%v + p%a*1e-10
-      p%pos = p%pos + p%v*1e-10
-
+      p%f = p%q*(E+cProd(p%v,B)) ! force change
+      p%a = p%f/p%m ! acceleration change
+      p%v = p%v + p%a*1e-10 ! velocity change
+      p%pos = p%pos + p%v*1e-10 ! position change
 
    end subroutine step
 
